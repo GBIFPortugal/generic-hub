3a2bd6e2a49d5a4ed444cd219261d4f3ad2360f9
rpfigueira <rui.figueira@iict.pt>
2016-07-25 17:05:46 +0100

    new message.properties and images

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -5,9 +5,14 @@ modules = {
 
     // Define your skin module here - it must 'dependsOn' either bootstrap (ALA version) or bootstrap2 (unmodified) and core
 
-    generic {
+//    generic {
+//        dependsOn 'bootstrap2', 'hubCore' //
+//        resource url: [dir:'css', file:'generic.css']
+//    }
+
+    pt {
         dependsOn 'bootstrap2', 'hubCore' //
-        resource url: [dir:'css', file:'generic.css']
+        resource url: [dir:'css', file:'generic-pt.css']
     }
 
 }
